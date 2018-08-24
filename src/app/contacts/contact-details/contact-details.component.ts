a9322c3e0bda8a7b8fa5930212c1d4c5491df853
Patrick Maciel <patrickmaciel.info@gmail.com>
2018-08-24 11:47:55 -0300

    Implementado todos os endpoints da API e criado a aplicação angular

---
+++ src/app/contacts/contact-details/contact-details.component.ts
@@ -0,0 +1,43 @@
+import {Component, Input, OnInit} from '@angular/core';
+import { Contact } from '../contact';
+import { ContactService } from '../contact.service';
+
+@Component({
+  selector: 'app-contact-details',
+  templateUrl: './contact-details.component.html',
+  styleUrls: ['./contact-details.component.css']
+})
+export class ContactDetailsComponent implements OnInit {
+  @Input()
+  contact: Contact;
+
+  @Input()
+  createHandler: Function;
+  @Input()
+  updateHandler: Function;
+  @Input()
+  deleteHandler: Function;
+
+  constructor(private contactService: ContactService) { }
+
+  ngOnInit() {
+  }
+
+  createContact(contact: Contact) {
+    this.contactService.createContact(contact).then((newContact: Contact) => {
+      this.createHandler(newContact);
+    });
+  }
+
+  updateContact(contact: Contact): void {
+    this.contactService.updateContact(contact).then((updateContact: Contact) => {
+      this.updateHandler(updateContact);
+    });
+  }
+
+  deleteContact(contactId: String): void {
+    this.contactService.deleteContact(contactId).then((deletedContactId: String) => {
+      this.deleteHandler(deletedContactId);
+    });
+  }
+}
